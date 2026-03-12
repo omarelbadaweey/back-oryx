95b086d6f1a3db0af00d72905e19e2e356f32d13
Omar Elbadaweey <omarelbadawey10@icloud.com>
2026-03-12 07:28:37 +0200

    edit data

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const contactRoute = require('./contactRoute');
 const commentRoute = require('./commentRoute ');
+const deviceTokenRoute = require('./deviceTokenRoute');
 const router = express.Router();
 
 router.use("/contact" , contactRoute)
 router.use("/comment" , commentRoute)
+router.use("/device-token" , deviceTokenRoute)
+
 module.exports = router;
